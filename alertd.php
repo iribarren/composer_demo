b87936df61c370363a3aca97eb0bb3266c4538e5
Aritz Iribarren Galarza <aritz.iribarren@scalefast.com>
2017-10-24 19:00:54 +0200

    fixes

--- alertd.php
+++ alertd.php
@@ -1,11 +1,14 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'config.php';
 
 define("FILEPATH", "/var/tmp/error.txt");
 
 // Create the Transport
-$transport = new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
+$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+  ->setUsername($username)
+  ->setPassword($password);
 
 $mailer = new Swift_Mailer($transport);
 
@@ -19,15 +22,21 @@ while (true) {
     if (file_exists(FILEPATH)) {
         echo "Error! enviando notificacion...".PHP_EOL;
         //send email
-        $message = (new Swift_Message('Wonderful Subject'))
+        $message = (new Swift_Message('alerta!'))
             ->setFrom(['alertas@alertas.com' => 'Mr Alerta'])
             ->setTo(['iribarren.aritz@gmail.com'])
-            ->setBody('Here is the message itself')
+            ->setBody('Hay un error!')
         ;
         $result   = $mailer->send($message);
-        
         //send slack notificacion
         $notifier = new SlackNotify\SlackNotifier();
+        $notifier = new SlackNotify\SlackNotifier();
+        $notifier->setSlackWebHookUrl( "https://hooks.slack.com/services/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 ");
+        $notifier->setText("Alerta!");
+        $notifier->setChannel("#aleatorio");
+        $notifier->setUserName("ALERT BOT");
+        $notifier->setIconEmoji(":robot_face:");
+        $notifier->notify();
         //once the nofitication is send wait 10 minutes to see that the error is fixed
         sleep(600);
     } else {
